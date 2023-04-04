b67cc35a13df24bb4b31227191deec64f18378e6
David <davidcitorojas15@gmail.com>
2023-04-04 16:21:42 -0400

    Algoritmos de kruskal y prim
    Los 2 algoritmos completados

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+from kruskal import kruskal
+from prim.prim import Prim
+
+nodes = ['a','b','c','d','e','f', 'g']
+edges = [
+    ('a', 'b', 7),
+    ('b', 'c', 8),
+    ('c', 'e', 5),
+    ('e', 'g', 9),
+    ('g', 'f', 11),
+    ('d', 'f', 6),
+    ('a', 'd', 5),
+    ('d', 'b', 9),
+    ('d', 'e', 15),
+    ('b', 'e', 7),
+    ('e', 'f', 8)
+]
+
+kruskal = kruskal.Kruskal()
+prim = Prim()
+
+node = random.choice(nodes)
+met = kruskal.apply_kruskal(nodes, edges)
+mst = prim.apply_prim(nodes, edges, node)
+
+print(met)
+print(node)
+#print(prim.sort_edges(mst))
+print(mst)
--- prim/prim.py
+++ prim/prim.py
@@ -1,9 +1,10 @@
-class Kruskal:
+class Prim:
 
     def __init__(self):
         self.mst = []
         self.nodes = {}
         self.levels = {}
+        self._available_edges = []
     
     def sort_edges(self, edges):
         return sorted(edges, key= lambda node: node[2])
@@ -25,18 +26,31 @@ class Kruskal:
             if self.levels[origin_root] == self.levels[destination_root]:
                 self.levels[destination_root] += 1
 
-    def apply_kruskal(self, nodes, edges):
+    def update_available_edges(self, node, edges):
+        for edge in edges: 
+            if edge not in self._available_edges and edge not in self.mst:
+                if edge[0] == node or edge[1] == node:
+                    self._available_edges.append(edge)
+        self._available_edges = self.sort_edges(self._available_edges)
+
+    def apply_prim(self, nodes, edges, actual_node):
         for node in nodes:
-            self.init_nl(node)
-        orded_edges = self.sort_edges(edges)
-        limit = len(nodes) - 1 
+            self.init_nl(node) 
+        self.update_available_edges(actual_node, edges)
+        limit = len(nodes) - 1
         i = 0
-        while len(self.mst) != limit:
-            origin, destination, weight = orded_edges[i]
+        while len(self.mst) < limit:
+            origin, destination, weight = self._available_edges[i]
             origin_root = self.find_root(origin)
             destination_root = self.find_root(destination)
             if origin_root != destination_root:
                 self.make_union(origin_root, destination_root)
-                self.mst.append(orded_edges[i])
-            i += 1
+                self.mst.append(self._available_edges[i])
+                self._available_edges.pop(i)
+                self.update_available_edges(origin, edges)
+                self.update_available_edges(destination, edges)
+                i = 0
+            else: 
+                i += 1
         return self.mst
+
