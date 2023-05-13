d85a0b11841984914d3a5bc62e5d02f1830d9229
joselct_ <work.jlct@gamil.com>
2023-05-13 01:15:42 -0400

    feat: practico 1 y 2

---
+++ met/main.py
@@ -0,0 +1,29 @@
+from kruskal import Kruskal
+from prim import Prim
+
+nodes = ['a','b','c','d','e','f', 'g']
+edges = [
+    ('a', 'b', 7),
+    ('b', 'c', 8),
+    ('c', 'e', 5),
+    ('e', 'g', 9),
+    ('g', 'f', 11),
+    ('d', 'f', 6),
+    ('a', 'd', 5),
+    ('d', 'b', 9),
+    ('d', 'e', 15),
+    ('b', 'e', 7),
+    ('e', 'f', 8)
+]
+
+
+if __name__ == '__main__':
+    kruskal = Kruskal()
+    prim = Prim()
+    metKruskal = kruskal.apply_kruskal(nodes, edges)
+    print("Kruskal")
+    print(metKruskal)
+    metPrim = prim.apply_prim(nodes, edges)
+    print("Prim")
+    print(metPrim)
+
